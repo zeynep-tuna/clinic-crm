8f4451c8e053a63e8ef26918a9c222aa652283a1
MrZa <beyzatuncer898@gmail.com>
2026-08-11 13:50:43 +0300

    Add Prisma service module

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
+      }),
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
